c197549fc2ae6a8b283cb5f250bd1fafb036e00d
Conor O'Brien <obrienceob@gmail.com>
2021-04-07 21:13:20 -0700

    added the session length and a little for the pets handlebars

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: 'Super Duper Secret',
+  cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
