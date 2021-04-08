35a5f5564a7472c927ad189a8e7be770424930c8
Conor O'Brien <obrienceob@gmail.com>
2021-04-08 11:53:43 -0700

    fixed add-pet.js

--- public/js/add-pet.js
+++ public/js/add-pet.js
@@ -6,16 +6,16 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#name').value.trim();
-    const animal_type = document.querySelector('#animal_type').value.trim();
+    const animal_type = $('input[name="animal_type"]:checked').val();;
     const breed = document.querySelector('#breed').value.trim();
     const description = document.querySelector('#description').value.trim();
-    const age = document.querySelector('#age').value.trim();
-    const gender = document.querySelector('#gender').value.trim();
-    const size = document.querySelector('#size').value.trim();
+    const age = $('input[name="age"]:checked').val();
+    const gender = $('input[name="gender"]:checked').val();;
+    const size = $('input[name="size"]:checked').val();;
     const good_with_kids = document.querySelector('#good_with_kids:checked') ? true : false;
     const good_with_dogs = document.querySelector('#good_with_dogs:checked') ? true : false;
     const good_with_cats = document.querySelector('#good_with_cats:checked') ? true : false;
-    const zodiac = document.querySelector('#zodiac').value.trim();
+    const zodiac = $('input[name="zodiac"]:checked').val();;
     const special_needs = document.querySelector('#special_needs:checked') ? true : false;
     const adoptable_now = document.querySelector('#good_with_dogs:checked') ? true : false;
     const contact_email = document.querySelector('#contact_email').value.trim();
