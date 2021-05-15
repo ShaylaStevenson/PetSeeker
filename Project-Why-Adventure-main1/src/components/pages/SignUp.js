9e374546640ee207b82d6cda2802c70f21fec086
Matache Marius <matachemarius92@gmail.com>
2021-05-15 14:02:46 -0700

    sing-up

---
+++ Project-Why-Adventure-main1/src/components/pages/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Form from '../singUp/Form';
+import FromSuccess from '../singUp/FormSuccess';
+
+
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1 className='sign-up'>sadsadsad</h1>;
+      <Form />
+      <FromSuccess />
+     </div>
+  ) 
+}
+
